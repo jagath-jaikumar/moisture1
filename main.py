9da0e8c92b94b5cab651e303eace86fe8ce89eb0
jjaikumar <jjaikumar@umass.edu>
2022-07-02 19:41:45 -0400

    cast wrapper

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=os.getenv("PORT", default=5000),
+        port=int(os.getenv("PORT", default="5000")),
         log_level="info",
     )
